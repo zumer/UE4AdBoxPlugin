e25a23910f38561c70651e4702d9a210190c0659
Evgeny <zubarev.evgeny@gmail.com>
2021-02-05 20:07:15 +0100

    Fixes to ads plugin

--- AdBox/Source/AdMob/AdMob.Build.cs
+++ AdBox/Source/AdMob/AdMob.Build.cs
@@ -2,61 +2,62 @@
 
 
 using System.IO;
+using UnrealBuildTool;
 
-namespace UnrealBuildTool.Rules
+public class AdMob : ModuleRules
 {
-	public class AdMob : ModuleRules
+	public AdMob(ReadOnlyTargetRules Target) : base(Target)
 	{
-		public AdMob(TargetInfo Target)
+		PublicIncludePaths.AddRange(
+			new string[] {
+				// ... add public include paths required here ...
+			}
+			);
+
+		PrivateIncludePaths.AddRange(
+			new string[] {
+				"AdMob/Private",
+				// ... add other private include paths required here ...
+			}
+			);
+
+		PublicDependencyModuleNames.AddRange(
+			new string[]
+			{					
+				// ... add other public dependencies that you statically link with here ...                  
+			}
+			);
+
+		PrivateDependencyModuleNames.AddRange(
+			new string[]
+			{
+				"Core",
+				"CoreUObject",                   
+				"Engine"                              
+				// ... add private dependencies that you statically link with here ...
+			}
+			);
+		
+		DynamicallyLoadedModuleNames.AddRange(
+			new string[]
+			{
+				// ... add any modules that your module loads dynamically here ...
+			}
+			);
+		if (Target.Platform == UnrealTargetPlatform.Android)
 		{
-			PublicIncludePaths.AddRange(
-				new string[] {
-					// ... add public include paths required here ...
-				}
-				);
-
-			PrivateIncludePaths.AddRange(
-				new string[] {
-					"AdMob/Private",
-					// ... add other private include paths required here ...
-				}
-				);
-
-			PublicDependencyModuleNames.AddRange(
-				new string[]
-				{					
-					// ... add other public dependencies that you statically link with here ...                  
-                }
-				);
-
 			PrivateDependencyModuleNames.AddRange(
-				new string[]
-				{
-                    "Core",
-                    "CoreUObject",                   
-                    "Engine"                              
-					// ... add private dependencies that you statically link with here ...
-				}
-				);
-           
-            DynamicallyLoadedModuleNames.AddRange(
-				new string[]
-				{
-					// ... add any modules that your module loads dynamically here ...
-				}
-				);
-            if (Target.Platform == UnrealTargetPlatform.Android)
-            {
-                PrivateDependencyModuleNames.AddRange(
-                new string[]
-                {
-                    "Launch"
-                }
-                );
+			new string[]
+			{
+				"Launch"
+			}
+			);
 
-                string PluginPath = Utils.MakePathRelativeTo(ModuleDirectory, BuildConfiguration.RelativeEnginePath);
-                AdditionalPropertiesForReceipt.Add(new ReceiptProperty("AndroidPlugin", Path.Combine(PluginPath, "AdMob_APL.xml")));
-            }
-        }
+			//string PluginPath = Utils.MakePathRelativeTo(ModuleDirectory, BuildConfiguration.RelativeEnginePath);
+			//AdditionalPropertiesForReceipt.Add(new ReceiptProperty("AndroidPlugin", Path.Combine(PluginPath, "AdMob_APL.xml")));
+   
+            string PluginPath = Utils.MakePathRelativeTo(ModuleDirectory, Target.RelativeEnginePath);
+            AdditionalPropertiesForReceipt.Add("AndroidPlugin", Path.Combine(PluginPath, "AdMob_APL.xml"));
+		}
 	}
 }
--- AdBox/Source/Chartboost/Chartboost.Build.cs
+++ AdBox/Source/Chartboost/Chartboost.Build.cs
@@ -2,61 +2,59 @@
 
 
 using System.IO;
+using UnrealBuildTool;
 
-namespace UnrealBuildTool.Rules
+public class Chartboost : ModuleRules
 {
-	public class Chartboost : ModuleRules
+	public Chartboost(ReadOnlyTargetRules Target) : base(Target)
 	{
-		public Chartboost(TargetInfo Target)
+		PublicIncludePaths.AddRange(
+			new string[] {
+				// ... add public include paths required here ...
+			}
+			);
+
+		PrivateIncludePaths.AddRange(
+			new string[] {
+				"Chartboost/Private",
+				// ... add other private include paths required here ...
+			}
+			);
+
+		PublicDependencyModuleNames.AddRange(
+			new string[]
+			{					
+				// ... add other public dependencies that you statically link with here ...                  
+			}
+			);
+
+		PrivateDependencyModuleNames.AddRange(
+			new string[]
+			{
+				"Core",
+				"CoreUObject",                   
+				"Engine"                              
+				// ... add private dependencies that you statically link with here ...
+			}
+			);
+		
+		DynamicallyLoadedModuleNames.AddRange(
+			new string[]
+			{
+				// ... add any modules that your module loads dynamically here ...
+			}
+			);
+		if (Target.Platform == UnrealTargetPlatform.Android)
 		{
-			PublicIncludePaths.AddRange(
-				new string[] {
-					// ... add public include paths required here ...
-				}
-				);
-
-			PrivateIncludePaths.AddRange(
-				new string[] {
-                    "Chartboost/Private",
-					// ... add other private include paths required here ...
-				}
-				);
-
-			PublicDependencyModuleNames.AddRange(
-				new string[]
-				{					
-					// ... add other public dependencies that you statically link with here ...                  
-                }
-				);
-
 			PrivateDependencyModuleNames.AddRange(
-				new string[]
-				{
-                    "Core",
-                    "CoreUObject",                   
-                    "Engine"                              
-					// ... add private dependencies that you statically link with here ...
-				}
-				);
-           
-            DynamicallyLoadedModuleNames.AddRange(
-				new string[]
-				{
-					// ... add any modules that your module loads dynamically here ...
-				}
-				);
-            if (Target.Platform == UnrealTargetPlatform.Android)
-            {
-                PrivateDependencyModuleNames.AddRange(
-                new string[]
-                {
-                    "Launch"
-                }
-                );
+			new string[]
+			{
+				"Launch"
+			}
+			);
 
-                string PluginPath = Utils.MakePathRelativeTo(ModuleDirectory, BuildConfiguration.RelativeEnginePath);
-                AdditionalPropertiesForReceipt.Add(new ReceiptProperty("AndroidPlugin", Path.Combine(PluginPath, "ChartBoost_APL.xml")));
-            }
-        }
+			string PluginPath = Utils.MakePathRelativeTo(ModuleDirectory, Target.RelativeEnginePath);
+            AdditionalPropertiesForReceipt.Add("AndroidPlugin", Path.Combine(PluginPath, "ChartBoost_APL.xml"));
+		}
 	}
 }
